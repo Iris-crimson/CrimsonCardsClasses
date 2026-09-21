b24e288f3e9a39ec720b0887cea59e66da5182b1
Iris-crimson <irispapandreou@protonmail.com>
2026-09-21 13:19:15 -0600

    Fixes

--- CrimsonCards/obj/Debug/netstandard2.1/CrimsonCards.AssemblyInfo.cs
+++ CrimsonCards/obj/Debug/netstandard2.1/CrimsonCards.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrimsonCards")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95eed5a74caedf717e94fa41747decb96cde6a15")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrimsonCards")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrimsonCards")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
